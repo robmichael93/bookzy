a4241344ea38ca306427acac4b3ea321708021bd
Rob Michael <robmichael93@gmail.com>
2023-02-25 00:37:36 -0600

    Update pom.xml and main Java file

--- src/test/java/com/simplilearn/bookzy/BookzyApplicationTests.java
+++ src/test/java/com/simplilearn/bookzy/BookzyApplicationTests.java
@@ -2,12 +2,45 @@ package com.simplilearn.bookzy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class BookzyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppConstructor() {
+        try {
+            new App();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testAppMain()
+    {
+        App.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
 }
